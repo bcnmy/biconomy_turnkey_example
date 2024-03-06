2acd4d3265c632a4673ee9d41c17dba0195c09c6
Venkatesh-Router <67518179+B-Venkatesh7210@users.noreply.github.com>
2024-03-06 23:58:44 +0530

    added Eth Sepolia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,16 @@
 import React, { useState } from "react";
-import { TurnkeySigner } from "@turnkey/ethers";
-import { ethers } from "ethers";
-import { TurnkeyClient } from "@turnkey/http";
-import { ApiKeyStamper } from "@turnkey/api-key-stamper";
-import {
-  ECDSAOwnershipValidationModule,
-  DEFAULT_ECDSA_OWNERSHIP_MODULE,
-} from "@biconomy/modules";
 import {
   createSmartAccountClient,
   BiconomySmartAccountV2,
   PaymasterMode,
 } from "@biconomy/account";
-import { IBundler, Bundler } from "@biconomy/bundler";
+import { ethers } from "ethers";
+import { TurnkeySigner } from "@turnkey/ethers";
+import { TurnkeyClient } from "@turnkey/http";
+import { ApiKeyStamper } from "@turnkey/api-key-stamper";
 import { contractABI } from "../contract/contractABI";
-
-// Create a provider for the Polygon Mumbai network
-const provider = new ethers.providers.JsonRpcProvider(
-  "https://rpc.ankr.com/polygon_mumbai"
-);
-
-// Specify the chain ID for Polygon Mumbai
-let chainId = 80001; // Polygon Mumbai or change as per your preferred chain
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   const [smartAccount, setSmartAccount] =
@@ -30,6 +19,28 @@ export default function Home() {
     null
   );
   const [count, setCount] = useState<string | null>(null);
+  const [txnHash, setTxnHash] = useState<string | null>(null);
+  const [chainSelected, setChainSelected] = useState<number>(0);
+
+  const chains = [
+    {
+      chainId: 11155111,
+      name: "Ethereum Sepolia",
+      providerUrl: "https://eth-sepolia.public.blastapi.io",
+      incrementCountContractAdd: "0xd9ea570eF1378D7B52887cE0342721E164062f5f",
+      biconomyPaymasterApiKey: "gJdVIBMSe.f6cc87ea-e351-449d-9736-c04c6fab56a2",
+      explorerUrl: "https://sepolia.etherscan.io/tx/",
+    },
+    {
+      chainId: 80001,
+      name: "Polygon Mumbai",
+      providerUrl: "https://rpc.ankr.com/polygon_mumbai",
+      incrementCountContractAdd: "0xc34E02663D5FFC7A1CeaC3081bF811431B096C8C",
+      biconomyPaymasterApiKey:
+        "-RObQRX9ei.fc6918eb-c582-4417-9d5a-0507b17cfe71",
+      explorerUrl: "https://mumbai.polygonscan.com/tx/",
+    },
+  ];
 
   const connect = async () => {
     try {
@@ -53,23 +64,24 @@ export default function Home() {
       });
 
       console.log(turnkeySigner, "TurnKey Signer");
-
-      console.log(provider, "Provider");
+      const provider = new ethers.providers.JsonRpcProvider(
+        chains[chainSelected].providerUrl
+      );
       const connectedSigner = turnkeySigner.connect(provider);
 
       console.log(connectedSigner, "Signer");
 
       const config = {
-        biconomyPaymasterApiKey:
-          "-RObQRX9ei.fc6918eb-c582-4417-9d5a-0507b17cfe71",
-        bundlerUrl: `https://bundler.biconomy.io/api/v2/${chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`, // <-- Read about this at https://docs.biconomy.io/dashboard#bundler-url
+        biconomyPaymasterApiKey: chains[chainSelected].biconomyPaymasterApiKey,
+        bundlerUrl: `https://bundler.biconomy.io/api/v2/${chains[chainSelected].chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`, // <-- Read about this at https://docs.biconomy.io/dashboard#bundler-url
       };
 
       const smartWallet = await createSmartAccountClient({
         signer: connectedSigner,
         biconomyPaymasterApiKey: config.biconomyPaymasterApiKey,
         bundlerUrl: config.bundlerUrl,
-        rpcUrl: "https://rpc.ankr.com/polygon_mumbai",
+        rpcUrl: chains[chainSelected].providerUrl,
+        chainId: chains[chainSelected].chainId,
       });
 
       console.log("Biconomy Smart Account", smartWallet);
@@ -83,7 +95,10 @@ export default function Home() {
   };
 
   const getCountId = async () => {
-    const contractAddress = "0xc34E02663D5FFC7A1CeaC3081bF811431B096C8C";
+    const contractAddress = chains[chainSelected].incrementCountContractAdd;
+    const provider = new ethers.providers.JsonRpcProvider(
+      chains[chainSelected].providerUrl
+    );
     const contractInstance = new ethers.Contract(
       contractAddress,
       contractABI,
@@ -94,33 +109,50 @@ export default function Home() {
   };
 
   const incrementCount = async () => {
-    const contractAddress = "0xc34E02663D5FFC7A1CeaC3081bF811431B096C8C";
-    const contractInstance = new ethers.Contract(
-      contractAddress,
-      contractABI,
-      provider
-    );
-    const minTx = await contractInstance.populateTransaction.increment();
-    console.log("Mint Tx Data", minTx.data);
-    const tx1 = {
-      to: contractAddress,
-      data: minTx.data,
-    };
+    try {
+      const toastId = toast("Populating Transaction", { autoClose: false });
 
-    //@ts-ignore
-    let userOp = await smartAccount?.buildUserOp([tx1], {
-      paymasterServiceData: { mode: PaymasterMode.SPONSORED },
-    });
-    console.log("UserOp", userOp);
+      const contractAddress = chains[chainSelected].incrementCountContractAdd;
+      const provider = new ethers.providers.JsonRpcProvider(
+        chains[chainSelected].providerUrl
+      );
+      const contractInstance = new ethers.Contract(
+        contractAddress,
+        contractABI,
+        provider
+      );
+      const minTx = await contractInstance.populateTransaction.increment();
+      console.log("Mint Tx Data", minTx.data);
+      const tx1 = {
+        to: contractAddress,
+        data: minTx.data,
+      };
 
-    //@ts-ignore
-    const userOpResponse = await smartAccount?.sendUserOp(userOp);
-    console.log("userOpHash", { userOpResponse });
-    //@ts-ignore
-    const { receipt } = await userOpResponse.wait(1);
-    console.log("txHash", receipt.transactionHash);
+      toast.update(toastId, {
+        render: "Sending Transaction",
+        autoClose: false,
+      });
+      //@ts-ignore
+      const userOpResponse = await smartAccount?.sendTransaction(tx1, {
+        paymasterServiceData: { mode: PaymasterMode.SPONSORED },
+      });
+      //@ts-ignore
+      const { transactionHash } = await userOpResponse.waitForTxHash();
+      console.log("Transaction Hash", transactionHash);
 
-    await getCountId();
+      if (transactionHash) {
+        toast.update(toastId, {
+          render: "Transaction Successful",
+          type: "success",
+          autoClose: 5000,
+        });
+        setTxnHash(transactionHash);
+        await getCountId();
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Transaction Unsuccessful", { autoClose: 5000 });
+    }
   };
 
   return (
@@ -129,12 +161,36 @@ export default function Home() {
         Biconomy Turnkey
       </div>
       {!smartAccount && (
-        <button
-          className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
-          onClick={connect}
-        >
-          Turnkey Sign in
-        </button>
+        <>
+          <div className="flex flex-row justify-center items-center gap-4">
+            <div
+              className={`w-[8rem] h-[3rem] cursor-pointer rounded-lg flex flex-row justify-center items-center text-white ${
+                chainSelected == 0 ? "bg-orange-600" : "bg-black"
+              } border-2 border-solid border-orange-400`}
+              onClick={() => {
+                setChainSelected(0);
+              }}
+            >
+              Eth Sepolia
+            </div>
+            <div
+              className={`w-[8rem] h-[3rem] cursor-pointer rounded-lg flex flex-row justify-center items-center text-white ${
+                chainSelected == 1 ? "bg-orange-600" : "bg-black"
+              } bg-black border-2 border-solid border-orange-400`}
+              onClick={() => {
+                setChainSelected(1);
+              }}
+            >
+              Poly Mumbai
+            </div>
+          </div>
+          <button
+            className="w-[10rem] h-[3rem] bg-orange-300 text-black font-bold rounded-lg"
+            onClick={connect}
+          >
+            Turnkey Sign in
+          </button>
+        </>
       )}
 
       {smartAccount && (
@@ -142,6 +198,7 @@ export default function Home() {
           {" "}
           <span>Smart Account Address</span>
           <span>{smartAccountAddress}</span>
+          <span>Network: {chains[chainSelected].name}</span>
           <div className="flex flex-row justify-between items-start gap-8">
             <div className="flex flex-col justify-center items-center gap-4">
               <button
@@ -159,8 +216,19 @@ export default function Home() {
               >
                 Increment Count
               </button>
+              {txnHash && (
+                <a
+                  target="_blank"
+                  href={`${chains[chainSelected].explorerUrl + txnHash}`}
+                >
+                  <span className="text-white font-bold underline">
+                    Txn Hash
+                  </span>
+                </a>
+              )}
             </div>
           </div>
+          <span className="text-white">Open console to view console logs.</span>
         </>
       )}
     </main>
